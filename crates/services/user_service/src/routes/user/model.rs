3ceec69ff941782d07aac855cd7ce3f172ed34d4
Aman Rao <amanrao@rapidinnovation.dev>
2025-01-24 16:00:35 +0530

    Added kafka integration to user service

--- crates/services/user_service/src/routes/user/model.rs
+++ crates/services/user_service/src/routes/user/model.rs
@@ -1,12 +1,11 @@
 use chrono::NaiveDateTime;
-use diesel::Queryable;
+use diesel::{Queryable, Selectable};
 use kafka::setup::KafkaTopic;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
-use crate::schema::users;
 
-#[derive(Queryable, Deserialize, Serialize, Debug)]
-#[diesel(table_name = users)]
+#[derive(Queryable, Deserialize, Serialize, Debug, Selectable)]
+#[diesel(table_name = crate::schema::users)]
 pub struct User {
     pub id: Uuid,
     pub username: String,
@@ -17,12 +16,25 @@ pub struct User {
 
 #[derive(Serialize)]
 pub struct RegisterUserMessage{
+    pub id: Uuid,
+    pub username: String,
     pub email: String,
-    pub name: String
+    pub created_at: Option<NaiveDateTime>
 }
 
 impl KafkaTopic for RegisterUserMessage {
     fn topic_name(&self) -> String {
         return "user_events".to_string()
+    }
+}
+
+impl From<User> for RegisterUserMessage {
+     fn from(value: User) -> Self {
+        Self {
+            id: value.id,
+            username: value.username,
+            email: value.email,
+            created_at: value.created_at
+        }
     }
 }
