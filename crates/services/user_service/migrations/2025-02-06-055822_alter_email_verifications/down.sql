dec858a32c31318273b1c3cbf91c8168bce24d06
Aman Rao <amanrao@rapidinnovation.dev>
2025-02-06 15:10:29 +0530

    Added down scripts to user migrations

--- crates/services/user_service/migrations/2025-02-06-055822_alter_email_verifications/down.sql
+++ crates/services/user_service/migrations/2025-02-06-055822_alter_email_verifications/down.sql
@@ -1,1 +1,7 @@
 -- This file should undo anything in `up.sql`
+
+ALTER TABLE email_verifications
+DROP COLUMN status;
+
+ALTER TABLE email_verifications
+ADD COLUMN status verification_status NOT NULL DEFAULT 'PENDING';
